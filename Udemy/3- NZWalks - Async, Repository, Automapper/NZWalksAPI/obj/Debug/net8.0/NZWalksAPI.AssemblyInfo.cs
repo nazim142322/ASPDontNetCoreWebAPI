5b9c983fc462fd84c8afb6f00f0d1eafac3297b0
Nazim <nazim142322@gmail.com>
2025-06-26 11:23:43 +0530

    CodeAdded

--- Udemy/3- NZWalks - Async, Repository, Automapper/NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
+++ Udemy/3- NZWalks - Async, Repository, Automapper/NZWalksAPI/obj/Debug/net8.0/NZWalksAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93c150a4fe7b2e5942c5ba64643182622f0e09f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140f9278387ea734dff2b802521a601584958cf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NZWalksAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
